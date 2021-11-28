1f24c9069ec358c59b53ada852e3edf18cf9087c
Nam Truong <dinhnamtruong090900@gmail.com>
2021-11-28 14:29:38 +0700

    bai thuc hanh so 3

--- app/src/main/java/com/example/donation10/MainActivity.java
+++ app/src/main/java/com/example/donation10/MainActivity.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button donateButton;
+    private RadioGroup paymentMethod;
+    private ProgressBar progressBar;
+    private NumberPicker amountPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         donateButton = findViewById(R.id.donateButton);
+        paymentMethod = findViewById(R.id.paymentMethod);
+        progressBar = findViewById(R.id.progressBar);
+        amountPicker = findViewById(R.id.amountPicker);
         if (donateButton != null)
         {
             Log.v("Donate", "Really got the donate button");
@@ -22,9 +31,15 @@ public class MainActivity extends AppCompatActivity {
         donateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("Donate", "Donate Pressed!");
+                donateButtonPressed(v);
             }
         });
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+    }
+    public void donateButtonPressed (View view)
+    {
+        Log.v("Donate", "Donate Pressed!");
     }
 
 }
