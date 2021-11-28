8b02da0080956de861d98a31a75101eb7d0d302a
Nam Truong <dinhnamtruong090900@gmail.com>
2021-11-28 13:59:11 +0700

    bai thuc hanh so 2

--- app/src/main/java/com/example/donation10/MainActivity.java
+++ app/src/main/java/com/example/donation10/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.donation10;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button donateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        donateButton = findViewById(R.id.donateButton);
+        if (donateButton != null)
+        {
+            Log.v("Donate", "Really got the donate button");
+        }
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("Donate", "Donate Pressed!");
+            }
+        });
     }
+
 }
